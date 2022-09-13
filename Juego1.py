e3d72468dfbab9b48c7ee442346cc92c0dd25381
PabloCeb2000 <113377312+PabloCeb2000@users.noreply.github.com>
2022-09-13 09:56:23 -0500

    Color
    Se agrego el color "purple" al los colores existentes

--- Juego1.py
+++ Juego1.py
@@ -59,6 +59,8 @@ onkey(lambda: color('white'), 'W')
 onkey(lambda: color('green'), 'G')
 onkey(lambda: color('blue'), 'B')
 onkey(lambda: color('red'), 'R')
+#Se agrego el color morado
+onkey(lambda: color('purple'), 'P')
 onkey(lambda: store('shape', line), 'l')
 onkey(lambda: store('shape', square), 's')
 onkey(lambda: store('shape', circle), 'c')
